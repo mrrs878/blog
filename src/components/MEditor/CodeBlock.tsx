22a3b76db38c7c2ddbab3a6d0d8eab9845769c4f
mrrs8 <mrrs878@foxmail.com>
2020-08-10 23:11:04 +0800

    添加docker-入门 完善markdown文章目录解析（代码块里有#也会生成标题）

--- src/components/MEditor/CodeBlock.tsx
+++ src/components/MEditor/CodeBlock.tsx
@@ -3,6 +3,7 @@ import hljs from 'highlight.js';
 import javascript from 'highlight.js/lib/languages/javascript';
 import html from 'highlight.js/lib/languages/vbscript-html';
 import ts from 'highlight.js/lib/languages/typescript';
+import shell from 'highlight.js/lib/languages/shell';
 import 'highlight.js/styles/github.css';
 
 hljs.registerLanguage('javascript', javascript);
@@ -11,6 +12,8 @@ hljs.registerLanguage('jsx', javascript);
 hljs.registerLanguage('html', html);
 hljs.registerLanguage('ts', ts);
 hljs.registerLanguage('typescript', ts);
+hljs.registerLanguage('shell script', shell);
+hljs.registerLanguage('shell', shell);
 
 interface PropsI {
   value: string;
--- src/views/article/index.tsx
+++ src/views/article/index.tsx
@@ -12,24 +12,25 @@ function getTitle(_content: string) {
   const tmpArr: Array<ContentI> = [];
   const content = clone(_content);
   let tmp: ContentI = { title: '', level: 2, key: '', children: [] };
-  content.replace(/(#+)[^#][^\n]*?(?:\n)/g, (match, m1) => {
-    const title = match.replace('\n', '')
-      .replace(/^#+/, '')
-      .replace(/\*+/g, '')
-      .replace(/\([^)]*?\)/, '');
-    const level = m1.length;
-    const item = {
-      title,
-      level,
-      key: title,
-      children: [],
-    };
-    if (level <= tmp.level) {
-      tmpArr.push(item);
-      tmp = item;
-    } else tmp.children.push(item);
-    return '';
-  });
+  content.replace(/```([\s\S]*)```/g, '')
+    .replace(/(#+)[^#][^\n]*?(?:\n)/g, (match, m1) => {
+      const title = match.replace('\n', '')
+        .replace(/^#+/, '')
+        .replace(/\*+/g, '')
+        .replace(/\([^)]*?\)/, '');
+      const level = m1.length;
+      const item = {
+        title,
+        level,
+        key: title,
+        children: [],
+      };
+      if (level <= tmp.level) {
+        tmpArr.push(item);
+        tmp = item;
+      } else tmp.children.push(item);
+      return '';
+    });
 
   return tmpArr;
 }
