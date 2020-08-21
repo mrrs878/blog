eb2d2c201861c10c2dacc4bcbc621105baa944ae
mrrs878 <mrrs878@foxmail.com>
2020-08-21 15:06:48 +0800

    添加docker-compose配置

--- update.sh
+++ update.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 echo "pulling image..."
-docker pull mrrs878/blog:latest
+docker-compose pull
 
-echo "stopping old app"
-docker container stop blog
+echo "stoping app..."
+docker-compose down
 
-echo "remove old container"
-docker container rm blog
-
-echo "crete new container"
-docker container create --name blog -p 8081:80 mrrs878/blog:latest
-
-echo "starting new app"
-docker container start blog
+echo "restarting app..."
+docker-compose up -d --build
 
 echo "awesome, you succeeded!"
