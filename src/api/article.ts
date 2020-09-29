842baddfceba5c1241bff00db3d6c9a5b506fd24
lihang.gw <lihang.gw@heyqu.net>
2020-09-29 12:35:12 +0800

    开发：触发Travis构建

--- src/api/article.ts
+++ src/api/article.ts
@@ -2,10 +2,6 @@
  * @Author: mrrs878
  * @LastEditors: Please set LastEditors
  */
-/*
- * @Author: mrrs878
- * @LastEditors: mrrs878
- */
 import ajax from '../tools/ajax';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || '/';
