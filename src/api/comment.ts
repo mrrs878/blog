ff8a06e4770fef53793fb2365ca1ff75fa98b2e4
mrrs8 <mrrs878@foxmail.com>
2020-10-08 23:32:41 +0800

    开发:更换获取评论接口

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -2,5 +2,5 @@ import ajax from '../tools/ajax';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || '/';
 
-export const GET_COMMENTS = (data: GetCommentsReqI): Promise<GetCommentsResI> => ajax.get(`${BASE_URL}/comment/${data.id}`);
+export const GET_COMMENTS = (data: GetCommentsReqI): Promise<GetCommentsResI> => ajax.get(`${BASE_URL}/comment/article/${data.id}`);
 export const ADD_COMMENT = (data: AddCommentReqI): Promise<AddCommentResI> => ajax.post(`${BASE_URL}/comment`, data);
