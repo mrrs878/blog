bd67ff720f9be16cf8e7ab4a1a2159f896c358a3
lihang.gw <lihang.gw@heyqu.net>
2020-11-02 22:46:28 +0800

    开发：修改获取所有文章接口

--- src/api/article.ts
+++ src/api/article.ts
@@ -6,5 +6,5 @@ import ajax from '../tools/ajax';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || '/';
 
-export const GET_ARTICLES = (): Promise<GetArticlesResI> => ajax.get(`${BASE_URL}/article/1`);
+export const GET_ARTICLES = (): Promise<GetArticlesResI> => ajax.get(`${BASE_URL}/article/all/1`);
 export const GET_ARTICLE = (data: GetArticlesReqI): Promise<GetArticleResI> => ajax.get(`${BASE_URL}/article/${data.id}`);
